c80760fcbc3777591576327f0954d6963ab73e3f
Sebastian Garrido <sebastian@ovou.com>
2024-10-09 00:49:05 -0500

    Add shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "tailwindcss";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   css: {
     postcss: {
       plugins: [tailwindcss()],
