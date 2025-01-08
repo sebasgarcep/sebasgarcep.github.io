396cf870210f902cc48431af7b92f5c6040c708b
Sebastian Garrido <sebasgarcep@gmail.com>
2025-01-08 14:57:55 -0500

    More work

--- src/pages/_index/Layout.tsx
+++ src/pages/_index/Layout.tsx
@@ -1,9 +1,9 @@
 import { Link, Outlet } from "react-router-dom";
-import { Mail, Newspaper, Tag, User } from "lucide-react";
+import { Mail, Menu, Newspaper, Tag, User } from "lucide-react";
 import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
 
-import { cn } from "@/lib/utils";
-
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import profileImg from "@/assets/profile.jpeg";
 
 interface MenuItem {
@@ -57,53 +57,78 @@ const socialItems: SocialItem[] = [
 
 export function Layout() {
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <div className="hidden border-r bg-muted/40 md:block">
+    <div className="bg-slate-800 grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <div className="hidden md:block">
         <div className="flex h-full max-h-screen flex-col gap-2">
-          <div className="flex-1">
-            <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-              <img src={profileImg} className="rounded-full w-36 h-36" />
-              <h1>{fullName}</h1>
-              <h2>{jobTitle}</h2>
-              {menuItems.map((item) => {
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-                const Icon = item.icon;
-                const selected = false;
-                return (
-                  <Link
-                    key={item.path}
-                    to={item.path}
-                    className={cn(
-                      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-                      selected
-                        ? "bg-muted text-primary"
-                        : "text-muted-foreground",
-                      "flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary",
-                    )}
-                  >
-                    <Icon className="h-4 w-4" />
-                    {item.label}
-                  </Link>
-                );
-              })}
-              {socialItems.map((item) => {
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-                const Icon = item.icon;
-                return (
-                  <Link key={item.path} to={item.path}>
-                    <Icon className="transition-all hover:text-primary h-4 w-4" />
-                  </Link>
-                );
-              })}
-            </nav>
-          </div>
+          <Sidebar />
         </div>
       </div>
       <div className="flex flex-col">
+        <header className="flex h-14 items-center gap-4 px-4 lg:h-[60px] lg:px-6">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button
+                variant="outline"
+                size="icon"
+                className="shrink-0 md:hidden"
+              >
+                <Menu className="h-5 w-5" />
+                <span className="sr-only">Toggle navigation menu</span>
+              </Button>
+            </SheetTrigger>
+            <SheetContent
+              side="left"
+              className="flex flex-col bg-slate-800 border-r-0"
+            >
+              <Sidebar />
+            </SheetContent>
+          </Sheet>
+        </header>
+
         <main>
           <Outlet />
         </main>
       </div>
     </div>
+  );
+}
+
+function Sidebar() {
+  return (
+    <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+      <img
+        src={profileImg}
+        className="rounded-full w-36 h-36 justify-self-center my-4"
+      />
+      <h1 className="text-gray-100 justify-self-center text-lg">{fullName}</h1>
+      <h2 className="text-gray-100 justify-self-center">{jobTitle}</h2>
+      <div className="mb-4" />
+      {menuItems.map((item) => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        const Icon = item.icon;
+        return (
+          <Link
+            key={item.path}
+            to={item.path}
+            className="flex items-center gap-3 rounded-lg px-3 py-2 transition-all text-gray-300 hover:text-gray-100"
+          >
+            <Icon className="h-4 w-4" />
+            {item.label}
+          </Link>
+        );
+      })}
+      <div className="mb-6" />
+      <div className="flex flex-row gap-8 justify-center">
+        {socialItems.map((item) => {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+          const Icon = item.icon;
+          return (
+            <Link key={item.path} to={item.path}>
+              <Icon className="transition-all text-gray-300 hover:text-gray-100 h-5 w-5" />
+            </Link>
+          );
+        })}
+      </div>
+    </nav>
   );
 }
